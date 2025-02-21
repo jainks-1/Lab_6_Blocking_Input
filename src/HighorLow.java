ac525a33b9a67a5d023e16ecc4fdf9efb30df190
jainks-1 <jainks@mail.uc.edu>
2025-02-21 12:09:34 -0500

    Added last two program outlines and desc

---
+++ src/HighorLow.java
@@ -0,0 +1,14 @@
+/*
+Write a program that has the computer generate a random int value between 1 to 10 inclusive.
+The program then asks the user to guess the number with a single guess.
+The program displays the random number and then indicates if the users guess was high low or on the money!
+Use the do while loop again to bulletproof the guess which must be an int from 1 to 10 inclusive.
+ */
+
+public class HighorLow {
+    public static void main(String[] args) {
+
+
+        System.exit(0);
+    }
+}
---
+++ src/RectangleInfo.java
@@ -0,0 +1,20 @@
+/*Write a program that asks the user for the lengths
+of the sides of a rectangle. Again, loop the input until
+it is valid.
+
+Note: Only two measures here, the Width and the Height should be doubles…
+Testing: use some simple values to confirm that the calculations are correct.
+
+Then print
+- The area and perimeter of the rectangle
+- The length of the diagonal (use the Pythagorean theorem)
+Note: a 3.0 by 4.0 rectangle will have a diagonal of 5.0
+*/
+
+public class RectangleInfo {
+    public static void main(String[] args) {
+
+
+        System.exit(0);
+    }
+}
