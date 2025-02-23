9ad7eb77244ee809755df1567015331abf2d4911
jainks-1 <jainks@mail.uc.edu>
2025-02-23 16:06:35 -0500

    Added input validation, need to code logic

--- src/HighorLow.java
+++ src/HighorLow.java
@@ -5,8 +5,47 @@ The program displays the random number and then indicates if the users guess was
 Use the do while loop again to bulletproof the guess which must be an int from 1 to 10 inclusive.
  */
 
+import java.util.Scanner;
+
 public class HighorLow {
     public static void main(String[] args) {
+
+        // Declare variables
+        Scanner in = new Scanner(System.in);
+
+        int userNum = 0;
+
+        boolean firstValid;
+
+        String trash = "";
+
+
+        // get user input, validate it
+        do {
+            System.out.println("Please enter your guess integer between [1-10]: ");
+            if (in.hasNextInt()) {
+                userNum = in.nextInt();
+                in.nextLine(); // Clear the buffer
+
+                if (userNum >= 1 && userNum <= 10) {
+
+                    firstValid = true;
+                } else {
+                    System.out.println("The input: [" + userNum + "] is invalid. Must be an int within [1-10].\n");
+
+                    firstValid = false;
+                }
+            } else {
+                trash = in.nextLine(); // Clear invalid input
+                System.out.println("The input: [" + trash + "] is invalid. Must be an int within [1-10].\n");
+
+                firstValid = false;
+            }
+        }
+        while (!firstValid);
+
+
+        System.out.println("Cont program");
 
 
         System.exit(0);
